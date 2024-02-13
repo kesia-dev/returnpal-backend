35d1bcc5fab01d43e4648f088943381538da3630
Angela Zhou <a03zhou@gmail.com>
2024-02-13 09:28:43 -0500

    create routes, functions, and add to the user schema for authentication features
    add check for password reset token
    
    fix nodemailer functionality, add to user schema, add forgot password mailer
    
    split nodemailer logic into different files as requested
    
    update comments, revise User schema to include username to allow saving
    
    remove code from authController that got moved to mailController
    
    add authorize route (work in progress)
    
    Added case insensitive check for email and fixed authorization route
    
    update using case insensitive email

--- models/user.js
+++ models/user.js
@@ -1,18 +1,55 @@
 const { mongoose } = require("mongoose");
 const userSchema = new mongoose.Schema({
-    username: {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+    firstName: {
       type: String,
       required: true,
-      unique: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
     },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    suiteNo: {
+      type: String,
+      required: false,
+    },
+    city: {
+      type: String,
+      required: false,
+    },
+    postalCode: {
+      type: String,
+      required: false,
+    },
     password: {
       type: String,
       required: true,
     },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    passwordResetToken: {
+      type: String,
+      required: false,
+    }
   });
   module.exports=mongoose.model('User', userSchema);
