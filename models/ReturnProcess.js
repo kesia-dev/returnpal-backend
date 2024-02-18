6bb8e93c3264acc4ae50b96d90d064032b272689
markmdoza <marklmdoza@gmail.com>
2024-02-18 14:16:58 -0500

    Add stripe schema

--- models/ReturnProcess.js
+++ models/ReturnProcess.js
@@ -1,186 +1,72 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-// Unsure if I should be using the same schema for every individual file..
-// Unsure how to save orders in which payment is successful through stripe.
-
+// ReturnProcessSchema
 const returnProcessSchema = new mongoose.Schema({
-    dateAndTime: {
-        type: Date,
-        required: true,
-    },
-    deliveryOption: {
-        type: String,
-        required: true,
-    },
-    price: {
-        name: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        itemId: {
-            type: String,
-            required: true,
-        },
+    dateAndTime: { type: Date, required: true },
+    deliveryOption: { type: String, required: true },
+    price: { 
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        itemId: { type: String, required: true },
     },
     confirmPickUp: {
         subscriptionItem: {
-            itemId: {
-                type: String,
-                required: true,
-                },
-            itemName: {
-                type: String,
-                required: true,
-                },
-            quantity: {
-                type: Number,
-                required: true,
-                },
+            itemId: { type: String, required: true },
+            itemName: { type: String, required: true },
+            quantity: { type: Number, required: true },
             },
         packageItem: {
-            itemId: {
-                type: String,
-                required: true,
-                },
-            itemName: {
-                type: String,
-                required: true,
-                },
-            quantity: {
-                type: Number,
-                required: true,
-                },
+            itemId: { type: String, required: true },
+            itemName: { type: String, required: true },
+            quantity: { type: Number, required: true },
             },
         expiryDate: {
-            orderDate: {
-                type: Date,
-                required: true,
-                },
-            subscription: {
-                type: String,
-                required: true,
-                },
+            orderDate: { type: Date, required: true },
+            subscription: { type: String, required: true },
             },
         calculateCost: {
-            subscription: {
-                type: String,
-                required: true,
-                },
-            packages: {
-                type: Number,
-                required: true,
-                },
+            subscription: { type: String, required: true },
+            packages: { type: Number, required: true },
         },
     },
     orderConfirmation: {
-        order: {
-            order_number: {
-                type: String,
-            },
-            order_date: {
-                type: Date,
-            },
-            order_status: {
-                type: String,
-            },
-            name: {
-                type: String,
-                required: true,
-            },
-            orderRef: {
-                type: String,
-                required: true,
-            },
-            email: {
-                type: String,
-                required: true,
-            },
-            location: {
-                type: String,
-                required: true,
-            },
-            cardType: {
-                type: String,
-                required: true,
-            },
-            cardNumber: {
-                type: Number,
-                required: true,
-            },
-            paymentSuccessful: {
-                type: Boolean,
-                required: true,
-            },
-            order_details: {
-                total_cost: {
-                    type: Number,
-                },
-                pickup_date: {
-                    type: Date,
-                    required: true,
-                },
-                pickup_method: {
-                    type: String,
-                    required: true,
-                },
-                total_packages: {
-                    type: Number,
-                },
-                extra_packages_included: {
-                    type: Number,
-                },
-                promo_code: {
-                    type: String,
-                },
-                pickup_details: {
-                    address_id: {
-                        ObjectId,
-                    },
-                    contact_full_name: {
-                        type: String,
-                        required: true,
-                    },
-                    contact_phone_number: {
-                        type: Number,
-                        required: true,
-                    },
-                    unit_number: {
-                        type: Number,
-                        required: true,
-                    },
-                    street: {
-                        type: String,
-                        required: true,
-                    },
-                    city: {
-                        type: String,
-                        required: true,
-                    },
-                    province: {
-                        type: String,
-                        required: true,
-                    },
-                    country: {
-                        type: String,
-                        required: true,
-                    },
-                    postal_code: {
-                        type: String,
-                        required: true,
-                    },
-                    instructions: {
-                        type: String,
-                        required: true,
-                    },
-                }
-            }
-        },
+        orderSchema,
     },
-})
+});
+
+// Stripe Schema
+const orderSchema = new Schema({
+        order_number: { type: String },
+        order_date: { type: Date },
+        order_status: { type: String },
+        name: { type: String, required: true },
+        orderRef: { type: String, required: true },
+        email: { type: String, required: true },
+        location: { type: String, required: true },
+        cardType: { type: String, required: true },
+        cardNumber: { type: Number, required: true },
+        paymentSuccessful: { type: Boolean, required: true },
+        order_details: {
+            total_cost: { type: Number },
+            pickup_date: { type: Date, required: true },
+            pickup_method: { type: String, required: true },
+            total_packages: { type: Number },
+            extra_packages_included: {type: Number },
+            promo_code: { type: String },
+            pickup_details: {   
+                address_id: { ObjectId },
+                contact_full_name: { type: String, required: true },
+                contact_phone_number: { type: Number, required: true },
+                unit_number: { type: Number, required: true },
+                street: { type: String, required: true },
+                city: { type: String, required: true },
+                province: { type: String, required: true },
+                country: { type: String, required: true },
+                postal_code: { type: String, required: true },
+                instructions: { type: String, required: true},
+            }
+        }
+    });
 
 module.exports = mongoose.model('returnProcess', returnProcessSchema);
