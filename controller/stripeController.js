2d3156c93fe11f6db462c2a216a4f56a9abae2cd
Ashwini <ashwinihegde28@gmail.com>
2024-02-29 23:46:07 -0500

    Backend code for Sripe Payment

--- controller/stripeController.js
+++ controller/stripeController.js
@@ -1,37 +1,42 @@
-const stripe = require("express");
-const express = require("stripe");
-
-process.env.STRIPE_SECRET_KEY;
+const express = require("express");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.charge = async (req, res) => {
-  try {
-    const { amount, source, receipt_email, promoCode } = req.body;
-    let chargeAmount = amount;
+  const { amount, receipt_email, promoCode } = req.body;
+  let chargeAmount = amount;
 
-    if (promoCode && promoCodes[promoCode]) {
-      const promoDetails = promoCodes[promoCode];
-      const discountPercentage = promoDetails.discountPercentage;
-      const discountAmount = (amount * discountPercentage) / 100;
-      chargeAmount -= discountAmount;
-    }
-
-    const charge = await stripe.charges.create({
-      amount: chargeAmount,
-      currency: "cad",
-      source,
-      receipt_email,
-      metadata: {
-        promoCode: promoCode || "No promo code applied",
-      },
-    });
-
-    res.status(200).json({
-      success: true,
-      charge,
-      discountApplied: promoCode ? true : false,
-    });
-  } catch (error) {
-    console.error("Error charging the customer:", error.message);
-    res.status(500).json({ error: error.message });
+  if (promoCode && promoCodes[promoCode]) {
+    const promoDetails = promoCodes[promoCode];
+    const discountPercentage = promoDetails.discountPercentage;
+    const discountAmount = (amount * discountPercentage) / 100;
+    chargeAmount -= discountAmount;
   }
+
+  const product = await stripe.products.create({
+    name: "P1",
+  });
+
+  const price = await stripe.prices.create({
+    unit_amount: chargeAmount * 100,
+    currency: "cad",
+    product: product.id,
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: price.id,
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    metadata: {
+      promoCode: promoCode || "No promo code applied",
+    },
+    success_url: `http://localhost:3000`,
+    cancel_url: `http://localhost:3000//schedule-pickup`,
+  });
+
+  res.json({ id: session.id });
 };
